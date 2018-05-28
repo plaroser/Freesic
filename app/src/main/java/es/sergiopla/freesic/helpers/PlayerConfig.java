285273c218611dc16765b5119c60942b850d799f
plaroser <sergio_pla_11@hotmail.com>
2018-05-28 21:11:23 +0200

    Busqueda video hecha solucionada

--- app/src/main/java/es/sergiopla/freesic/helpers/PlayerConfig.java
+++ app/src/main/java/es/sergiopla/freesic/helpers/PlayerConfig.java
@@ -1,6 +1,6 @@
 package es.sergiopla.freesic.helpers;
 
 public class PlayerConfig {
-    public static final String API_KEY = "AIzaSyD8y25--eJYM-_lMsUYnle7wraUYsIALRA";
+    public static final String API_KEY = "AIzaSyDz1pqkPjClj58_X05H1zgNSyahW3CICE8";
 }
 
